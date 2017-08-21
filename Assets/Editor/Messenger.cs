a72bb723c4d65f64a18510ff4ac73c437b67ecd1
canyon <gongyanghui1986@163.com>
2017-08-21 20:30:27 +0800

    添加三个参数的方法体

--- Assets/Editor/Messenger.cs
+++ Assets/Editor/Messenger.cs
@@ -55,7 +55,7 @@ static public class Messenger
 		}
 	}
 
-	static public void Invoke(string eventType)
+	static void Invoke(string eventType)
 	{
 		Delegate d;
 		// Invoke the delegate only if the event type is in the dictionary.
@@ -72,7 +72,6 @@ static public class Messenger
 			}
 		}
 	}
-	
 
 	static public void Brocast(string eventType){
 		Invoke (eventType);
@@ -80,31 +79,24 @@ static public class Messenger
 
 	static public void AddListener<T>(string eventType, System.Action<T> handler)
 	{
-		// Obtain a lock on the event table to keep this thread-safe.
 		lock (eventTable)
 		{
-			// Create an entry for this event type if it doesn't already exist.
 			if (!eventTable.ContainsKey(eventType))
 			{
 				eventTable.Add(eventType, null);
 			}
-			// Add the handler to the event.
 			eventTable[eventType] = (System.Action<T>)eventTable[eventType] + handler;
 		}
 	}
 
 	static public void RemoveListener<T>(string eventType, System.Action<T> handler)
 	{
-		// Obtain a lock on the event table to keep this thread-safe.
 		lock (eventTable)
 		{
-			// Only take action if this event type exists.
 			if (eventTable.ContainsKey(eventType))
 			{
-				// Remove the event handler from this event.
 				eventTable[eventType] = (System.Action<T>)eventTable[eventType] - handler;
 
-				// If there's nothing left then remove the event type from the event table.
 				if (eventTable[eventType] == null)
 				{
 					eventTable.Remove(eventType);
@@ -113,17 +105,13 @@ static public class Messenger
 		}
 	}
 
-	static public void Invoke<T>(string eventType, T arg1)
+	static void Invoke<T>(string eventType, T arg1)
 	{
 		Delegate d;
-		// Invoke the delegate only if the event type is in the dictionary.
 		if (eventTable.TryGetValue(eventType, out d))
 		{
-			// Take a local copy to prevent a race condition if another thread
-			// were to unsubscribe from this event.
 			System.Action<T> callback = (System.Action<T>)d;
 
-			// Invoke the delegate if it's not null.
 			if (callback != null)
 			{
 				callback(arg1);
@@ -137,31 +125,24 @@ static public class Messenger
 
 	static public void AddListener<T, U>(string eventType, System.Action<T, U> handler)
 	{
-		// Obtain a lock on the event table to keep this thread-safe.
 		lock (eventTable)
 		{
-			// Create an entry for this event type if it doesn't already exist.
 			if (!eventTable.ContainsKey(eventType))
 			{
 				eventTable.Add(eventType, null);
 			}
-			// Add the handler to the event.
 			eventTable[eventType] = (System.Action<T, U>)eventTable[eventType] + handler;
 		}
 	}
 
 	static public void RemoveListener<T, U>(string eventType, System.Action<T, U> handler)
 	{
-		// Obtain a lock on the event table to keep this thread-safe.
 		lock (eventTable)
 		{
-			// Only take action if this event type exists.
 			if (eventTable.ContainsKey(eventType))
 			{
-				// Remove the event handler from this event.
 				eventTable[eventType] = (System.Action<T, U>)eventTable[eventType] - handler;
 
-				// If there's nothing left then remove the event type from the event table.
 				if (eventTable[eventType] == null)
 				{
 					eventTable.Remove(eventType);
@@ -170,17 +151,13 @@ static public class Messenger
 		}
 	}
 
-	static public void Invoke<T, U>(string eventType, T arg1, U arg2)
+	static void Invoke<T, U>(string eventType, T arg1, U arg2)
 	{
 		Delegate d;
-		// Invoke the delegate only if the event type is in the dictionary.
 		if (eventTable.TryGetValue(eventType, out d))
 		{
-			// Take a local copy to prevent a race condition if another thread
-			// were to unsubscribe from this event.
 			System.Action<T, U> callback = (System.Action<T, U>)d;
 
-			// Invoke the delegate if it's not null.
 			if (callback != null)
 			{
 				callback(arg1, arg2);
@@ -190,5 +167,46 @@ static public class Messenger
 
 	static public void Brocast<T, U>(string eventType, T arg1, U arg2){
 		Invoke (eventType,arg1,arg2);
+	}
+
+	static public void AddListener<T1,T2,T3>(string eventType, System.Action<T1,T2,T3> handler)
+	{
+		lock (eventTable)
+		{
+			if (!eventTable.ContainsKey(eventType))
+			{
+				eventTable.Add(eventType, null);
+			}
+			eventTable[eventType] = (System.Action<T1,T2,T3>)eventTable[eventType] + handler;
+		}
+	}
+
+	static public void RemoveListener<T1,T2,T3>(string eventType, System.Action<T1,T2,T3> handler)
+	{
+		lock (eventTable)
+		{
+			if (eventTable.ContainsKey(eventType))
+			{
+				eventTable[eventType] = (System.Action<T1,T2,T3>)eventTable[eventType] - handler;
+
+				if (eventTable[eventType] == null)
+				{
+					eventTable.Remove(eventType);
+				}
+			}
+		}
+	}
+
+	static public void Brocast<T1,T2,T3>(string eventType, T1 arg1, T2 arg2, T3 arg3){
+		Delegate d;
+		if (eventTable.TryGetValue(eventType, out d))
+		{
+			System.Action<T1,T2,T3> callback = (System.Action<T1,T2,T3>)d;
+
+			if (callback != null)
+			{
+				callback(arg1, arg2,arg3);
+			}
+		}
 	}
 }
