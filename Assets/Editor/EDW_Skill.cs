377a5c49164bc31b039dfc2f90dd99c2cf0f9176
canyon <gongyanghui1986@163.com>
2017-08-21 20:23:18 +0800

    开始绘制，准备绘制结构为， 左中右，中分上下结构，先只绘制结构，绘制一个时间线出来

---
+++ Assets/Editor/EDW_Skill.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+/// <summary>
+/// 类名 : 技能编辑器窗口Class
+/// 作者 : Canyon
+/// 日期 : 2017-01-05 17:10
+/// 功能 : 
+/// </summary>
+public class EDW_Skill : EditorWindow
+{
+    static bool isOpenWindowView = false;
+
+    static protected EDW_Skill vwWindow = null;
+
+	static int leftWidth = 200;
+	static int midMinWidth = 200;
+	static int rightWidth = 220;
+
+    // 窗体宽高
+	static public float width = leftWidth + midMinWidth + rightWidth + 30;
+    static public float height = 480;
+
+    [MenuItem("Tools/Windows/EDSkill")]
+    static void AddWindow()
+    {
+        if (isOpenWindowView || vwWindow != null)
+            return;
+
+        try
+        {
+            isOpenWindowView = true;
+
+            // 大小不能拉伸
+            // vwWindow = GetWindowWithRect<EDW_Skill>(rect, true, "SkillEditor");
+            
+            // 窗口，只能单独当成一个窗口,大小可以拉伸
+            //vwWindow = GetWindow<EDW_Skill>(true,"SkillEditor");
+
+            // 这个合并到其他窗口中去,大小可以拉伸
+            vwWindow = GetWindow<EDW_Skill>("SkillEditor");
+
+			Vector2 pos = new Vector2(100,100);
+			Vector2 size = new Vector2(width,height);
+			Rect rect = new Rect(pos,size);
+            vwWindow.position = rect;
+			vwWindow.minSize = size;
+        }
+        catch (System.Exception)
+        {
+            OnClearSWindow();
+            throw;
+        }
+    }
+
+    static void OnClearSWindow()
+    {
+        isOpenWindowView = false;
+        vwWindow = null;
+    }
+
+    static public float Round(float org, int acc)
+    {
+        float pow = Mathf.Pow(10, acc);
+        float temp = org * pow;
+        return Mathf.RoundToInt(temp) / pow;
+    }
+
+	static void CheckApplicationPlaying(){
+		if (!EditorApplication.isPlaying) {
+			EditorApplication.isPlaying = true;
+		}
+	}
+
+    #region  == Member Attribute ===
+
+    #endregion
+
+    #region  == EditorWindow Func ===
+
+    void Awake()
+    {
+        Init();
+    }
+
+    void OnEnable()
+    {
+        EditorApplication.update += OnUpdate;
+    }
+
+    void OnDisable()
+    {
+        EditorApplication.update -= OnUpdate;
+    }
+
+    void OnGUI()
+    {
+        
+    }
+
+    // 在给定检视面板每秒10帧更新
+    void OnInspectorUpdate()
+    {
+        Repaint();
+    }
+
+    void OnDestroy()
+    {
+        OnClearSWindow();
+        EditorApplication.update -= OnUpdate;
+
+        Clear();
+    }
+
+    #endregion
+
+    #region  == Self Func ===
+
+	/// <summary>
+	/// 创建当前行对象的位置
+	/// </summary>
+	Rect CreateRect(ref int nX,int nY,int nWidth,int nHeight = 20){
+		Rect rect = new Rect (nX, nY, nWidth, nHeight);
+		nX += nWidth + 5;
+		return rect;
+	}
+
+	/// <summary>
+	/// 设置下一行的开始位置
+	/// </summary>
+	void NextLine(ref int nX,ref int nY,int addHeight = 30,int resetX = 10){
+		nX = resetX;
+		nY += addHeight;
+	}
+
+    void Init()
+    {
+    }
+
+    void OnUpdate()
+    {
+    }
+
+    void Clear()
+    {
+    }
+    #endregion
+}
---
+++ Assets/Editor/Messenger.cs
@@ -0,0 +1,194 @@
+﻿// Messenger.cs v0.1 (20090925) by Rod Hyde (badlydrawnrod).
+//
+// This is a C# messenger (notification center) for Unity. It uses delegates
+// and generics to provide type-checked messaging between event producers and
+// event consumers, without the need for producers or consumers to be aware of
+// each other.
+
+// http://wiki.unity3d.com/index.php/CSharpMessenger
+// Modify by Canyon
+// Time : 2017-02-07
+
+using System;
+using System.Collections.Generic;
+
+
+/**
+ * A messenger for events that have no parameters.
+ */
+static public class Messenger
+{
+	private static Dictionary<string, Delegate> eventTable = new Dictionary<string, Delegate>();
+
+	static public void AddListener(string eventType, System.Action handler)
+	{
+		// Obtain a lock on the event table to keep this thread-safe.
+		lock (eventTable)
+		{
+			// Create an entry for this event type if it doesn't already exist.
+			if (!eventTable.ContainsKey(eventType))
+			{
+				eventTable.Add(eventType, null);
+			}
+			// Add the handler to the event.
+			eventTable[eventType] = (System.Action)eventTable[eventType] + handler;
+		}
+	}
+
+	static public void RemoveListener(string eventType, System.Action handler)
+	{
+		// Obtain a lock on the event table to keep this thread-safe.
+		lock (eventTable)
+		{
+			// Only take action if this event type exists.
+			if (eventTable.ContainsKey(eventType))
+			{
+				// Remove the event handler from this event.
+				eventTable[eventType] = (System.Action)eventTable[eventType] - handler;
+
+				// If there's nothing left then remove the event type from the event table.
+				if (eventTable[eventType] == null)
+				{
+					eventTable.Remove(eventType);
+				}
+			}
+		}
+	}
+
+	static public void Invoke(string eventType)
+	{
+		Delegate d;
+		// Invoke the delegate only if the event type is in the dictionary.
+		if (eventTable.TryGetValue(eventType, out d))
+		{
+			// Take a local copy to prevent a race condition if another thread
+			// were to unsubscribe from this event.
+			System.Action callback = (System.Action) d;
+
+			// Invoke the delegate if it's not null.
+			if (callback != null)
+			{
+				callback();
+			}
+		}
+	}
+	
+
+	static public void Brocast(string eventType){
+		Invoke (eventType);
+	}
+
+	static public void AddListener<T>(string eventType, System.Action<T> handler)
+	{
+		// Obtain a lock on the event table to keep this thread-safe.
+		lock (eventTable)
+		{
+			// Create an entry for this event type if it doesn't already exist.
+			if (!eventTable.ContainsKey(eventType))
+			{
+				eventTable.Add(eventType, null);
+			}
+			// Add the handler to the event.
+			eventTable[eventType] = (System.Action<T>)eventTable[eventType] + handler;
+		}
+	}
+
+	static public void RemoveListener<T>(string eventType, System.Action<T> handler)
+	{
+		// Obtain a lock on the event table to keep this thread-safe.
+		lock (eventTable)
+		{
+			// Only take action if this event type exists.
+			if (eventTable.ContainsKey(eventType))
+			{
+				// Remove the event handler from this event.
+				eventTable[eventType] = (System.Action<T>)eventTable[eventType] - handler;
+
+				// If there's nothing left then remove the event type from the event table.
+				if (eventTable[eventType] == null)
+				{
+					eventTable.Remove(eventType);
+				}
+			}
+		}
+	}
+
+	static public void Invoke<T>(string eventType, T arg1)
+	{
+		Delegate d;
+		// Invoke the delegate only if the event type is in the dictionary.
+		if (eventTable.TryGetValue(eventType, out d))
+		{
+			// Take a local copy to prevent a race condition if another thread
+			// were to unsubscribe from this event.
+			System.Action<T> callback = (System.Action<T>)d;
+
+			// Invoke the delegate if it's not null.
+			if (callback != null)
+			{
+				callback(arg1);
+			}
+		}
+	}
+
+	static public void Brocast<T>(string eventType, T arg1){
+		Invoke (eventType,arg1);
+	}
+
+	static public void AddListener<T, U>(string eventType, System.Action<T, U> handler)
+	{
+		// Obtain a lock on the event table to keep this thread-safe.
+		lock (eventTable)
+		{
+			// Create an entry for this event type if it doesn't already exist.
+			if (!eventTable.ContainsKey(eventType))
+			{
+				eventTable.Add(eventType, null);
+			}
+			// Add the handler to the event.
+			eventTable[eventType] = (System.Action<T, U>)eventTable[eventType] + handler;
+		}
+	}
+
+	static public void RemoveListener<T, U>(string eventType, System.Action<T, U> handler)
+	{
+		// Obtain a lock on the event table to keep this thread-safe.
+		lock (eventTable)
+		{
+			// Only take action if this event type exists.
+			if (eventTable.ContainsKey(eventType))
+			{
+				// Remove the event handler from this event.
+				eventTable[eventType] = (System.Action<T, U>)eventTable[eventType] - handler;
+
+				// If there's nothing left then remove the event type from the event table.
+				if (eventTable[eventType] == null)
+				{
+					eventTable.Remove(eventType);
+				}
+			}
+		}
+	}
+
+	static public void Invoke<T, U>(string eventType, T arg1, U arg2)
+	{
+		Delegate d;
+		// Invoke the delegate only if the event type is in the dictionary.
+		if (eventTable.TryGetValue(eventType, out d))
+		{
+			// Take a local copy to prevent a race condition if another thread
+			// were to unsubscribe from this event.
+			System.Action<T, U> callback = (System.Action<T, U>)d;
+
+			// Invoke the delegate if it's not null.
+			if (callback != null)
+			{
+				callback(arg1, arg2);
+			}
+		}
+	}
+
+	static public void Brocast<T, U>(string eventType, T arg1, U arg2){
+		Invoke (eventType,arg1,arg2);
+	}
+}
